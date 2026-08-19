0080ca944bb070adb479775080ca1a65e8f386d2
Héllida Louize <hellida.llgomes@gmail.com>
2026-08-19 11:15:33 -0300

    Refactor Main.java to use Plano objects

--- src/principais/Main.java
+++ src/principais/Main.java
@@ -4,6 +4,11 @@ import classesDeMidia.Faixa;
 import classesDeMidia.Midia;
 import organizacao.Album;
 import organizacao.Playlist;
+import planos.Gratuito;
+import planos.Plano;
+import planos.PlanoEstudante;
+import planos.PlanoFamily;
+import planos.PlanoIndividual;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,14 +36,14 @@ public class Main {
         System.out.print("Opcao: ");
         int opcaoPlano = Integer.parseInt(scanner.nextLine());
 
-        String tipoPlano = switch (opcaoPlano) {
-            case 2 -> "Individual";
-            case 3 -> "Estudante";
-            case 4 -> "Familia";
-            default -> "Gratuito";
+        Plano plano = switch (opcaoPlano) {
+            case 2 -> new PlanoIndividual(19.90);
+            case 3 -> new PlanoEstudante(9.90);
+            case 4 -> new PlanoFamily(34.90);
+            default -> new Gratuito(0.0);
         };
 
-        Usuario usuario = new Usuario(nome, tipoPlano);
+        Usuario usuario = new Usuario(nome, plano);
         System.out.println("Usuario " + usuario.getNome() + " cadastrado com sucesso!");
 
         System.out.print("\nDigite o nome para a sua Playlist: ");
@@ -64,20 +69,25 @@ public class Main {
                     for (int i = 0; i < catalogoGlobal.size(); i++) {
                         Midia m = catalogoGlobal.get(i);
                         if (m instanceof Faixa f) {
-                            System.out.printf("%d. %s - %s [%s]\n", (i + 1), f.getTitulo(), f.getArtista(), f.getTags());
+                            System.out.printf("%d. %s - %s [%s]\n", (i + 1), f.getNome(), f.getArtista(), f.getTags());
                         }
                     }
                 }
                 case 2 -> {
                     System.out.println("\n--- ADICIONAR A PLAYLIST ---");
                     for (int i = 0; i < catalogoGlobal.size(); i++) {
-                        System.out.printf("%d. %s - %s\n", (i + 1), catalogoGlobal.get(i).getTitulo(), ((Faixa) catalogoGlobal.get(i)).getArtista());
+                        Midia m = catalogoGlobal.get(i);
+                        if (m instanceof Faixa f) {
+                            System.out.printf("%d. %s - %s\n", (i + 1), f.getNome(), f.getArtista());
+                        } else {
+                            System.out.printf("%d. %s\n", (i + 1), m.getNome());
+                        }
                     }
                     System.out.print("Digite o numero da musica: ");
                     int num = Integer.parseInt(scanner.nextLine());
                     if (num > 0 && num <= catalogoGlobal.size()) {
                         playlistUsuario.adicionarItem(catalogoGlobal.get(num - 1));
-                        System.out.println("'" + catalogoGlobal.get(num - 1).getTitulo() + "' adicionada com sucesso!");
+                        System.out.println("'" + catalogoGlobal.get(num - 1).getNome() + "' adicionada com sucesso!");
                     } else {
                         System.out.println("[ERRO] Opcao invalida!");
                     }
@@ -87,7 +97,11 @@ public class Main {
                     if (playlistUsuario.getItens().isEmpty()) {
                         System.out.println("(Sua playlist esta vazia)");
                     } else {
-                        playlistUsuario.getItens().forEach(item -> System.out.println("- " + item.getTitulo()));
+                        playlistUsuario.getItens().forEach(item -> {
+                            if (item instanceof Midia m) {
+                                System.out.println("- " + m.getNome());
+                            }
+                        });
                     }
                 }
                 case 4 -> player.iniciarReproducao(usuario, playlistUsuario, catalogoGlobal);
@@ -116,51 +130,51 @@ public class Main {
         Album albumTchaikovsky = new Album("1812 Overture", "Pyotr Ilyich Tchaikovsky");
         Album albumShostakovich = new Album("Symphony No. 5", "Dmitri Shostakovich");
 
-        Faixa r1 = new Faixa("Chop Suey!", "210", "System of a Down", albumSoad);
+        Faixa r1 = new Faixa("Chop Suey!", 210, "System of a Down", albumSoad);
         r1.adicionarTag("Rock");
         r1.adicionarTag("Metal");
 
-        Faixa r2 = new Faixa("Toxicity", "219", "System of a Down", albumSoad);
+        Faixa r2 = new Faixa("Toxicity", 219, "System of a Down", albumSoad);
         r2.adicionarTag("Rock");
         r2.adicionarTag("Metal");
 
-        Faixa r3 = new Faixa("Wind of Change", "312", "Scorpions", albumScorpions);
+        Faixa r3 = new Faixa("Wind of Change", 312, "Scorpions", albumScorpions);
         r3.adicionarTag("Rock");
         r3.adicionarTag("Classic Rock");
 
-        Faixa r4 = new Faixa("Rock You Like a Hurricane", "255", "Scorpions", albumScorpions);
+        Faixa r4 = new Faixa("Rock You Like a Hurricane", 255, "Scorpions", albumScorpions);
         r4.adicionarTag("Rock");
         r4.adicionarTag("Hard Rock");
 
-        Faixa r5 = new Faixa("Bohemian Rhapsody", "354", "Queen", albumQueen);
+        Faixa r5 = new Faixa("Bohemian Rhapsody", 354, "Queen", albumQueen);
         r5.adicionarTag("Rock");
 
-        Faixa r6 = new Faixa("Don't Stop Me Now", "209", "Queen", albumQueen);
+        Faixa r6 = new Faixa("Don't Stop Me Now", 209, "Queen", albumQueen);
         r6.adicionarTag("Rock");
 
-        Faixa p1 = new Faixa("Story of My Life", "245", "One Direction", album1D);
+        Faixa p1 = new Faixa("Story of My Life", 245, "One Direction", album1D);
         p1.adicionarTag("Pop");
         p1.adicionarTag("Pop Rock");
 
-        Faixa p2 = new Faixa("What Makes You Beautiful", "200", "One Direction", album1D);
+        Faixa p2 = new Faixa("What Makes You Beautiful", 200, "One Direction", album1D);
         p2.adicionarTag("Pop");
 
-        Faixa m1 = new Faixa("You Don't Know Me", "230", "Caetano Veloso", albumCaetano);
+        Faixa m1 = new Faixa("You Don't Know Me", 230, "Caetano Veloso", albumCaetano);
         m1.adicionarTag("MPB");
 
-        Faixa m2 = new Faixa("Sampa", "185", "Caetano Veloso", albumCaetano);
+        Faixa m2 = new Faixa("Sampa", 185, "Caetano Veloso", albumCaetano);
         m2.adicionarTag("MPB");
 
-        Faixa s1 = new Faixa("Deixa a Vida Me Levar", "270", "Zeca Pagodinho", albumZeca);
+        Faixa s1 = new Faixa("Deixa a Vida Me Levar", 270, "Zeca Pagodinho", albumZeca);
         s1.adicionarTag("Samba");
 
-        Faixa s2 = new Faixa("OGUM", "290", "Zeca Pagodinho", albumZeca);
+        Faixa s2 = new Faixa("OGUM", 290, "Zeca Pagodinho", albumZeca);
         s2.adicionarTag("Samba");
 
-        Faixa c1 = new Faixa("1812 Overture, Op. 49", "900", "Pyotr Ilyich Tchaikovsky", albumTchaikovsky);
+        Faixa c1 = new Faixa("1812 Overture, Op. 49", 900, "Pyotr Ilyich Tchaikovsky", albumTchaikovsky);
         c1.adicionarTag("Classica");
 
-        Faixa c2 = new Faixa("Waltz No. 2", "220", "Dmitri Shostakovich", albumShostakovich);
+        Faixa c2 = new Faixa("Waltz No. 2", 220, "Dmitri Shostakovich", albumShostakovich);
         c2.adicionarTag("Classica");
 
         catalogo.add(r1);
