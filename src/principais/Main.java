1fd752a5dba1a388b679ca888761587e01287fe9
Héllida Louize <hellida.llgomes@gmail.com>
2026-08-19 11:08:01 -0300

    Refactor plan handling and Faixa constructor parameters
    Updated the Main.java file to change the type of plan from object instances to string representations. Modified the Faixa constructor calls to accept string durations instead of integers.

--- src/principais/Main.java
+++ src/principais/Main.java
@@ -4,11 +4,6 @@ import classesDeMidia.Faixa;
 import classesDeMidia.Midia;
 import organizacao.Album;
 import organizacao.Playlist;
-import planos.Gratuito;
-import planos.Plano;
-import planos.PlanoEstudante;
-import planos.PlanoFamily;
-import planos.PlanoIndividual;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,14 +31,14 @@ public class Main {
         System.out.print("Opcao: ");
         int opcaoPlano = Integer.parseInt(scanner.nextLine());
 
-        Plano plano = switch (opcaoPlano) {
-            case 2 -> new PlanoIndividual();
-            case 3 -> new PlanoEstudante();
-            case 4 -> new PlanoFamily();
-            default -> new Gratuito();
+        String tipoPlano = switch (opcaoPlano) {
+            case 2 -> "Individual";
+            case 3 -> "Estudante";
+            case 4 -> "Familia";
+            default -> "Gratuito";
         };
 
-        Usuario usuario = new Usuario(nome, plano);
+        Usuario usuario = new Usuario(nome, tipoPlano);
         System.out.println("Usuario " + usuario.getNome() + " cadastrado com sucesso!");
 
         System.out.print("\nDigite o nome para a sua Playlist: ");
@@ -121,51 +116,51 @@ public class Main {
         Album albumTchaikovsky = new Album("1812 Overture", "Pyotr Ilyich Tchaikovsky");
         Album albumShostakovich = new Album("Symphony No. 5", "Dmitri Shostakovich");
 
-        Faixa r1 = new Faixa("Chop Suey!", 210, "System of a Down", albumSoad);
+        Faixa r1 = new Faixa("Chop Suey!", "210", "System of a Down", albumSoad);
         r1.adicionarTag("Rock");
         r1.adicionarTag("Metal");
 
-        Faixa r2 = new Faixa("Toxicity", 219, "System of a Down", albumSoad);
+        Faixa r2 = new Faixa("Toxicity", "219", "System of a Down", albumSoad);
         r2.adicionarTag("Rock");
         r2.adicionarTag("Metal");
 
-        Faixa r3 = new Faixa("Wind of Change", 312, "Scorpions", albumScorpions);
+        Faixa r3 = new Faixa("Wind of Change", "312", "Scorpions", albumScorpions);
         r3.adicionarTag("Rock");
         r3.adicionarTag("Classic Rock");
 
-        Faixa r4 = new Faixa("Rock You Like a Hurricane", 255, "Scorpions", albumScorpions);
+        Faixa r4 = new Faixa("Rock You Like a Hurricane", "255", "Scorpions", albumScorpions);
         r4.adicionarTag("Rock");
         r4.adicionarTag("Hard Rock");
 
-        Faixa r5 = new Faixa("Bohemian Rhapsody", 354, "Queen", albumQueen);
+        Faixa r5 = new Faixa("Bohemian Rhapsody", "354", "Queen", albumQueen);
         r5.adicionarTag("Rock");
 
-        Faixa r6 = new Faixa("Don't Stop Me Now", 209, "Queen", albumQueen);
+        Faixa r6 = new Faixa("Don't Stop Me Now", "209", "Queen", albumQueen);
         r6.adicionarTag("Rock");
 
-        Faixa p1 = new Faixa("Story of My Life", 245, "One Direction", album1D);
+        Faixa p1 = new Faixa("Story of My Life", "245", "One Direction", album1D);
         p1.adicionarTag("Pop");
         p1.adicionarTag("Pop Rock");
 
-        Faixa p2 = new Faixa("What Makes You Beautiful", 200, "One Direction", album1D);
+        Faixa p2 = new Faixa("What Makes You Beautiful", "200", "One Direction", album1D);
         p2.adicionarTag("Pop");
 
-        Faixa m1 = new Faixa("You Don't Know Me", 230, "Caetano Veloso", albumCaetano);
+        Faixa m1 = new Faixa("You Don't Know Me", "230", "Caetano Veloso", albumCaetano);
         m1.adicionarTag("MPB");
 
-        Faixa m2 = new Faixa("Sampa", 185, "Caetano Veloso", albumCaetano);
+        Faixa m2 = new Faixa("Sampa", "185", "Caetano Veloso", albumCaetano);
         m2.adicionarTag("MPB");
 
-        Faixa s1 = new Faixa("Deixa a Vida Me Levar", 270, "Zeca Pagodinho", albumZeca);
+        Faixa s1 = new Faixa("Deixa a Vida Me Levar", "270", "Zeca Pagodinho", albumZeca);
         s1.adicionarTag("Samba");
 
-        Faixa s2 = new Faixa("OGUM", 290, "Zeca Pagodinho", albumZeca);
+        Faixa s2 = new Faixa("OGUM", "290", "Zeca Pagodinho", albumZeca);
         s2.adicionarTag("Samba");
 
-        Faixa c1 = new Faixa("1812 Overture, Op. 49", 900, "Pyotr Ilyich Tchaikovsky", albumTchaikovsky);
+        Faixa c1 = new Faixa("1812 Overture, Op. 49", "900", "Pyotr Ilyich Tchaikovsky", albumTchaikovsky);
         c1.adicionarTag("Classica");
 
-        Faixa c2 = new Faixa("Waltz No. 2", 220, "Dmitri Shostakovich", albumShostakovich);
+        Faixa c2 = new Faixa("Waltz No. 2", "220", "Dmitri Shostakovich", albumShostakovich);
         c2.adicionarTag("Classica");
 
         catalogo.add(r1);
